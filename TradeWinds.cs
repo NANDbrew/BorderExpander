7670aa7222ea8918b7e21a325a6b935e7c8f0375
NANDbrew <zortman@gmail.com>
2025-05-14 03:31:58 -0500

    move trade wind handling into its own class, add region priority sorting

---
+++ TradeWinds.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BorderExpander
+{
+    public class TradeWinds
+    {
+        internal static float defaultTradeWindInfluence;
+        private static List<TradeWindRegion> tradeWindRegions = new List<TradeWindRegion>();
+
+        public static void AddRegion(TradeWindRegion region)
+        {
+            tradeWindRegions.Add(region);
+            tradeWindRegions.Sort((a, b) => a.GetArea().CompareTo(b.GetArea()));
+        }
+
+        public static TradeWindRegion GetCurrentTradeWindRegion()
+        {
+            float longitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).x;
+            float latitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).z;
+
+            foreach (var region in tradeWindRegions)
+            {
+                if (longitude > region.westBorder && longitude < region.eastBorder && latitude > region.southBorder && latitude < region.northBorder)
+                {
+                    return region;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class TradeWindRegion
+    {
+        public float northBorder = 0f;
+        public float southBorder = 0f;
+        public float eastBorder = 0f;
+        public float westBorder = 0f;
+        public Vector3 direction = Vector3.zero;
+        public float influence = 0.25f;
+    
+        public float GetArea() 
+        {
+            return (northBorder - southBorder) * (eastBorder - westBorder); 
+        }
+    }
+}
--- WindPatch.cs
+++ WindPatch.cs
@@ -11,44 +11,26 @@ namespace BorderExpander
     [HarmonyPatch(typeof(Wind))]
     public static class WindPatch
     {
-        internal static float defaultTradeWindInfluence;
-        public static List<TradeWindRegion> tradeWindRegions = new List<TradeWindRegion>();
-
-        [HarmonyPatch("Start")]
+        [HarmonyPatch("Awake")]
         [HarmonyPostfix]
-        private static void StartPatch(float ___tradeWindInfluence)
+        private static void AwakePatch(float ___tradeWindInfluence)
         {
-            defaultTradeWindInfluence = ___tradeWindInfluence;
+            TradeWinds.defaultTradeWindInfluence = ___tradeWindInfluence;
         }
 
         [HarmonyPatch("GetCurrentTradeWind")]
         [HarmonyPrefix]
         public static bool GetTradeWindPatch(ref Vector3 __result, ref float ___tradeWindInfluence)
         {
-            float longitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).x;
-            float latitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).z;
-
-            foreach (var region in tradeWindRegions)
+            if (TradeWinds.GetCurrentTradeWindRegion() is TradeWindRegion region)
             {
-                if (longitude > region.westBorder && longitude < region.eastBorder && latitude > region.southBorder && latitude < region.northBorder)
-                {
-                    ___tradeWindInfluence = region.influence;
-                    __result = region.direction.normalized;
-                    return false;
-                }
+                ___tradeWindInfluence = region.influence;
+                __result = region.direction.normalized;
+                return false;
             }
-            ___tradeWindInfluence = defaultTradeWindInfluence;
+            ___tradeWindInfluence = TradeWinds.defaultTradeWindInfluence;
             return true;
         }
     }
 
-    public class TradeWindRegion
-    {
-        public float northBorder = 0f;
-        public float southBorder = 0f;
-        public float eastBorder = 0f;
-        public float westBorder = 0f;
-        public Vector3 direction = Vector3.zero;
-        public float influence = 0.25f;
-    }
 }
