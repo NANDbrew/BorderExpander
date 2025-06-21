99332dbb67114f5df93ea5e3c0c3ef677fc86da4
NANDbrew <zortman@gmail.com>
2025-06-21 17:09:27 -0500

    Change TradeWindRegion to use Unity's Rect for borders

--- TradeWinds.cs
+++ TradeWinds.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BorderExpander;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,37 +16,41 @@ namespace BorderExpander
         public static void AddRegion(TradeWindRegion region)
         {
             tradeWindRegions.Add(region);
-            tradeWindRegions.Sort((a, b) => a.GetArea().CompareTo(b.GetArea()));
+            //tradeWindRegions.Sort((a, b) => a.GetArea().CompareTo(b.GetArea()));
+            tradeWindRegions.Sort((a, b) => a.Area.CompareTo(b.Area));
         }
 
         public static TradeWindRegion GetCurrentTradeWindRegion()
         {
-            float longitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).x;
-            float latitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).z;
+/*            float longitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).x;
+            float latitude = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform).z;*/
+            var pos = FloatingOriginManager.instance.GetGlobeCoords(Refs.observerMirror.transform);
+            Vector2 coords = new Vector2(pos.x, pos.z);
 
             foreach (var region in tradeWindRegions)
             {
-                if (longitude > region.westBorder && longitude < region.eastBorder && latitude > region.southBorder && latitude < region.northBorder)
+/*                if (longitude > region.westBorder && longitude < region.eastBorder && latitude > region.southBorder && latitude < region.northBorder)
                 {
                     return region;
-                }
+                }*/
+                if (region.limits.Contains(coords)) return region;
             }
-            return null;
+            return default;
         }
     }
 
-    public class TradeWindRegion
+    public struct TradeWindRegion
     {
-        public float northBorder = 0f;
-        public float southBorder = 0f;
-        public float eastBorder = 0f;
-        public float westBorder = 0f;
-        public Vector3 direction = Vector3.zero;
-        public float influence = 0.25f;
-    
-        public float GetArea() 
+        public Rect limits;
+        public Vector3 direction;
+        public float influence;// = 0.25f;
+        public float Area => limits.width * limits.height;
+        //public static TradeWindRegion Default => new TradeWindRegion { limits = Rect.zero, direction = Vector3.right, influence = 0.25f};
+/*        public float GetArea()
         {
-            return (northBorder - southBorder) * (eastBorder - westBorder); 
-        }
+            //if (limits.Contains(Vector2.one)
+            //return (northBorder - southBorder) * (eastBorder - westBorder);
+            TradeWinds.AddRegion(new TradeWindRegion { limits = Rect.MinMaxRect(-10, 0, 10, 10), direction = Vector3.right, influence = 0.5f });
+        }*/
     }
 }
--- WindPatch.cs
+++ WindPatch.cs
@@ -11,18 +11,42 @@ namespace BorderExpander
     [HarmonyPatch(typeof(Wind))]
     public static class WindPatch
     {
+        static TradeWindRegion northern = new TradeWindRegion 
+        { 
+            limits = Rect.MinMaxRect(float.NegativeInfinity, 33f, float.PositiveInfinity, float.PositiveInfinity),
+            direction = new Vector3(1f, 0f, 0.5f), 
+            influence = 0.25f 
+        };
+        static TradeWindRegion alankh = new TradeWindRegion 
+        { 
+            limits = Rect.MinMaxRect(float.NegativeInfinity, 30f, 33f, -2f), 
+            direction = new Vector3(0.75f, 0f, 0.75f), 
+            influence = 0.25f 
+        };
+        static TradeWindRegion emerald = new TradeWindRegion 
+        { 
+            limits = Rect.MinMaxRect(-2f, 30f, 32f, float.PositiveInfinity), 
+            direction = new Vector3(-1f, 0f, -0.5f), 
+            influence = 0.25f 
+        };
+
         [HarmonyPatch("Awake")]
         [HarmonyPostfix]
-        private static void AwakePatch(float ___tradeWindInfluence)
+        public static void AwakePatch(float ___tradeWindInfluence)
         {
             TradeWinds.defaultTradeWindInfluence = ___tradeWindInfluence;
+
+            TradeWinds.AddRegion(northern);
+            TradeWinds.AddRegion(alankh);
+            TradeWinds.AddRegion(emerald);
         }
 
         [HarmonyPatch("GetCurrentTradeWind")]
         [HarmonyPrefix]
         public static bool GetTradeWindPatch(ref Vector3 __result, ref float ___tradeWindInfluence)
         {
-            if (TradeWinds.GetCurrentTradeWindRegion() is TradeWindRegion region)
+            var region = TradeWinds.GetCurrentTradeWindRegion();
+            if (region.limits != null)
             {
                 ___tradeWindInfluence = region.influence;
                 __result = region.direction.normalized;
